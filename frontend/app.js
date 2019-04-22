78c99c45e3e96a74f9cc04bec0e1be7f43c58b87
M 🔥🔥🔥 <>
2019-04-22 23:02:52 +0100

    MEH... Wasted some energy reinventing wheel. Time to sleep. Moon soon.

--- frontend/app.js
+++ frontend/app.js
@@ -509,12 +509,9 @@ app.controller('HomeCtrl', function($scope, $q) {
   $scope.message = "WORK STARTED";
   $scope.challenges = [];
 
-  setTimeout(async function() {
-
+  setTimeout(async function() { // HACK HACK HACK
     let temp = await contract.getChallengesCount();
-
     let challengesCount = temp.toNumber()
-  
     let arrayOfPromises = [];
   
     for (let i=0; i<challengesCount; i++) {
@@ -524,164 +521,14 @@ app.controller('HomeCtrl', function($scope, $q) {
     $q.all(arrayOfPromises).then(function(results) {
       console.log(results);
       results.forEach((r) => {
-
-        // console.log( r[0], r[1].toNumber(), r[2], r[3].toNumber(), r[4].toNumber(), r[5].toNumber(), r[6] );
-
         $scope.challenges.push(new Challenge(r[0], parseFloat(ethers.utils.formatEther(r[1])).toFixed(3), r[2], r[3].toNumber(), r[4].toNumber(), r[5].toNumber(), r[6] ))
       })
-      // $scope.$apply();
     })
 
   }, 1000); // HACK HACK HACK
-
-
-
-
-
-
-
-
-
-  // $scope.warningShown = true;
-	// $scope.accounts = [];
-  // $scope.bids = [];
-  // $scope.guranteedBids = [];
-	// $scope.refunds = [];
-
-  // $scope.contract.description.call(function(err, res) {
-  //   $scope.description = res;
-  //   $scope.$apply();
-  // });  
-
-  // $scope.contract.timestampEnd.call(function(err, res) {
-  //   $scope.timestampEnd = res.toNumber();
-  //   $scope.$apply();
-  // });  
-
-  // $scope.contract.howManyGuaranteed.call(function(err, res) {
-  //   $scope.howManyGuaranteed = res.toNumber();
-  // });    
-
-  // $scope.contract.price.call(function(err, res) {
-  //   $scope.price = +web3.fromWei( res.toNumber() );
-  //   $scope.$apply();
-  // });    
-
-  // $scope.contract.priceGuaranteed.call(function(err, res) {
-  //   $scope.priceGuaranteed = +web3.fromWei( res.toNumber() );
-  // });  
-
-  // $scope.contract.howMany.call(function(err, res) {
-  //   $scope.howMany = res.toNumber();
-  // });
-
-  // $scope.contract.getAccountListLenght(function(err, res) {
-  //   lenght = res.toNumber() + 1; // because we have HEAD and TAIL we do some shananigans here
-  //   for (i=0; i<lenght; i++) {
-  //     $scope.contract.bids(i, function(err2, res2) {
-
-  //       console.log(res2);
-
-  //       if(res2[3] === "0x0000000000000000000000000000000000000000") return; // dropping witdrawn / HEAD / TAIL bids
-
-  //       var account = {
-  //         bidder: res2[3],
-  //         value: +web3.fromWei(res2[2].toNumber()) // BigNumber to Number to Ether to digits...
-  //       }
-
-  //       $scope.accounts.push(account);  
-  //       $scope.$apply(); // TODO: promises, $apply only once towards the end
-
-  //     });
-  //   }
-  // });  
-
-  // $scope.contract.getGuaranteedContributorsLenght(function(err, res) {
-  //   $scope.guaranteedSold = lenght = res.toNumber()
-  //   for (i=0; i<lenght; i++) {
-  //     $scope.contract.guaranteedContributors(i, function(err2, res2) {
-  //       $scope.contract.guaranteedContributions(res2, function(err3, res3) {
-
-  //         var guaranteed = {
-  //           bidder: res2,
-  //           value: +web3.fromWei(res3.toNumber()) // BigNumber to Number to Ether to digits...
-  //         }
-  //         $scope.guranteedBids.push(guaranteed);
-  //         $scope.$apply();
-  //       })
-  //     });
-  //   }
-  // });
-
-  // let bidEvent = $scope.contract.BidEvent({}, {fromBlock: 0, toBlock: 'latest'})
-  // bidEvent.get(function(error, events) {
-
-  //   console.log(events);
-
-  //   events.forEach(function(event) {
-  //     var bid = {
-  //       bidder: event.args.bidder,
-  //       value: +web3.fromWei( event.args.value.toNumber() ),
-  //       timestamp: event.args.timestamp.toNumber(),
-  //       tx: event.transactionHash
-  //     }
-  //     $scope.bids.push(bid);
-  //   });
-
-  //   $scope.$apply();
-  // });   
-
-  // let guaranteedBidEvent = $scope.contract.GuaranteedBid({}, {fromBlock: 0, toBlock: 'latest'})
-  // guaranteedBidEvent.get(function(error, events) {
-
-  //   console.log(events);
-
-  //   events.forEach(function(event) {
-  //     var bid = {
-  //       bidder: event.args.bidder,
-  //       value: +web3.fromWei( event.args.value.toNumber() ),
-  //       timestamp: event.args.timestamp.toNumber(),
-  //       tx: event.transactionHash
-  //     }
-  //     $scope.bids.push(bid);
-  //   });
-
-  //   $scope.$apply();
-  // });  
-
-
-  // let refundEvent = $scope.contract.Refund({}, {fromBlock: 0, toBlock: 'latest'})
-  // refundEvent.get(function(error, events) {
-
-  //   console.log(events);
-
-  // 	events.forEach(function(event) {
-  // 		var refund = {
-  // 			bidder: event.args.bidder,
-  // 			value: +web3.fromWei( event.args.value.toNumber() ),
-  // 			timestamp: event.args.timestamp.toNumber(),
-  // 			tx: event.transactionHash
-  // 		}
-  // 		$scope.refunds.push(refund);
-  // 	});
-
-  // 	$scope.$apply();
-  // });
-
-
-
-  // TODO: display popup as new bid is incoming
-  // BidEvent = Instance.BidEvent();
-  // BidEvent.watch(function(error, result){
-  //  console.log(error, result);
-  // });
-
 });
-
 
 app.controller('ChallengeCtrl', function($scope, $routeParams) {
   console.log($routeParams);
   $scope.id = $routeParams.id;
-
-
 });
