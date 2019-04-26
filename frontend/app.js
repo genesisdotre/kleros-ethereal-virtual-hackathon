8a82aeabc482ccfb6a0cff7c72bfca272cb7fbe1
M 🔥🔥🔥 <>
2019-04-26 16:39:21 +0100

    Using old version of Bootstrap to be compatible with Angular

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('app', ['angularMoment', 'ngRoute'])
+var app = angular.module('app', ['angularMoment', 'ngRoute', 'ui.bootstrap'])
 
 class Challenge {
   constructor(user, deposit, description, beginning, end, count, state) {
@@ -50,25 +50,38 @@ app.run(async function($rootScope) {
   
   $rootScope.ABI = [{"constant":true,"inputs":[{"name":"","type":"address"},{"name":"","type":"uint256"}],"name":"userChallengeIDs","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"user","type":"address"}],"name":"getUserChallengeIDs","outputs":[{"name":"","type":"uint256[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"},{"name":"","type":"uint256"}],"name":"chalengeSubmissionIDs","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"submissionID","type":"uint256"}],"name":"getSubmissionById","outputs":[{"name":"","type":"string"},{"name":"","type":"string"},{"name":"","type":"uint256"},{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"getSubmissionsCount","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"getChallengesCount","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"renounceOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_description","type":"string"},{"name":"_beginning","type":"uint256"},{"name":"_end","type":"uint256"},{"name":"_count","type":"uint256"}],"name":"createChallenge","outputs":[{"name":"","type":"uint256"}],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"challenges","outputs":[{"name":"user","type":"address"},{"name":"deposit","type":"uint256"},{"name":"description","type":"string"},{"name":"beginning","type":"uint256"},{"name":"end","type":"uint256"},{"name":"count","type":"uint256"},{"name":"state","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"isOwner","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"challengeID","type":"uint256"}],"name":"getChallengeById","outputs":[{"name":"","type":"address"},{"name":"","type":"uint256"},{"name":"","type":"string"},{"name":"","type":"uint256"},{"name":"","type":"uint256"},{"name":"","type":"uint256"},{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"submissions","outputs":[{"name":"url","type":"string"},{"name":"comment","type":"string"},{"name":"timestamp","type":"uint256"},{"name":"state","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"challengeID","type":"uint256"}],"name":"getChallengeSubmissionIDs","outputs":[{"name":"","type":"uint256[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_url","type":"string"},{"name":"_comment","type":"string"},{"name":"challengeID","type":"uint256"}],"name":"createSubmission","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"creator","type":"address"},{"indexed":false,"name":"deposit","type":"uint256"},{"indexed":false,"name":"description","type":"string"},{"indexed":false,"name":"beginning","type":"uint256"},{"indexed":false,"name":"end","type":"uint256"},{"indexed":false,"name":"count","type":"uint256"},{"indexed":false,"name":"timestamp","type":"uint256"}],"name":"ChallengeAdd","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"name":"creator","type":"address"},{"indexed":false,"name":"challengeID","type":"uint256"},{"indexed":false,"name":"url","type":"string"},{"indexed":false,"name":"comment","type":"string"},{"indexed":false,"name":"timestamp","type":"uint256"}],"name":"SubmissionAdd","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"previousOwner","type":"address"},{"indexed":true,"name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"}]
 
-  try {
-    accounts = await ethereum.enable();
-  } catch (error) {
-    console.log(error);
-  }
+  // try {
+  //   accounts = await ethereum.enable();
+  // } catch (error) {
+  //   console.log(error);
+  // }
 
-  provider = new ethers.providers.Web3Provider(web3.currentProvider);
+  // provider = new ethers.providers.Web3Provider(web3.currentProvider);
 
-  console.log(await provider.getNetwork());
+  // console.log(await provider.getNetwork());
 
-  signer = provider.getSigner();
+  // signer = provider.getSigner();
 
-  contract = new ethers.Contract($rootScope.address, $rootScope.ABI, signer);
+  // contract = new ethers.Contract($rootScope.address, $rootScope.ABI, signer);
 
 });
 
 app.controller('HomeCtrl', function($scope, $q) {
   $scope.message = "WORK STARTED";
   $scope.challenges = [];
+
+  $scope.alerts = [
+    { type: 'danger', msg: 'Oh snap! Change a few things up and try submitting again.' },
+    { type: 'success', msg: 'Well done! You successfully read this important alert message.' }
+  ];
+
+  $scope.addAlert = function() {
+    $scope.alerts.push({msg: 'Another alert!'});
+  };
+
+  $scope.closeAlert = function(index) {
+    $scope.alerts.splice(index, 1);
+  };
 
   $scope.form = {
     deposit: "0.01",
