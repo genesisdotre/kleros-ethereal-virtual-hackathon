573bd917a3760396654b46c95255525894177fbe
M <>
2019-05-05 17:13:55 +0100

    some tests, WIP

--- test/selfCommitment.js
+++ test/selfCommitment.js
@@ -7,25 +7,37 @@ const CentralizedArbitrator = artifacts.require('./CentralizedArbitrator.sol')
 contract('SelfCommitment', async function(accounts) {
     const sender = accounts[0]
     const creator1 = accounts[1];
-    const creator2 = accounts[1];
+    const creator2 = accounts[2];
+    const troll = accounts[3]; // internet troll who is disputing a submissiono.
     const arbitratorOwner = accounts[5]
     const now = Math.floor( (+new Date()) / 1000 );
     const day = 24 * 60 * 60;
+    const minute = 60
     const ETH = 10**18;
 
-    let centralizedArbitrator;
+    let arbitrator;
     let selfCommitment;
 
     beforeEach(async function() {
-        centralizedArbitrator = await CentralizedArbitrator.new(0.1 * ETH, { from: arbitratorOwner } );
-        selfCommitment = await SelfCommitment.new(centralizedArbitrator.address, [], { from: sender } );
+        arbitrator = await CentralizedArbitrator.new(0.1 * ETH, { from: arbitratorOwner } );
+        selfCommitment = await SelfCommitment.new(arbitrator.address, [], { from: sender } );
     })
   
     it('Should create a first challenge and craete two submissions', async () => {
-        await selfCommitment.createChallenge("20 pushups", now - 4*day, now - 4*day, 20, { from: creator1, value: 18**10 });
+        await selfCommitment.createChallenge("20 pushups", now, now + 4*day, 20, { from: creator1, value: ETH });
         let challengesCount = await selfCommitment.getChallengesCount();
         assert.equal(challengesCount.toNumber(), 1, 'There should be exactly one challenge');
-        
+
+        let c = await selfCommitment.getChallengeById(0); // using short letters to avoid typing
+
+        assert.equal(c[0], creator1, "creator")
+        assert.equal(c[1].toNumber(), ETH, "deposit");
+        assert.equal(c[2], "20 pushups", "description");
+        assert.equal(c[3].toNumber(), now, "start");
+        assert.equal(c[4].toNumber(), now + 4*day, "end");
+        assert.equal(c[5].toNumber(), 20, "count");
+        assert.equal(c[6].toNumber(), 0, "state");
+
         await selfCommitment.createSubmission("url", "something", 0, { from: creator1 });
         let submissionsCount = await selfCommitment.getSubmissionsCount();
         assert.equal(submissionsCount.toNumber(), 1, 'There should be exactly one submission');
@@ -38,41 +50,34 @@ contract('SelfCommitment', async function(accounts) {
         let secondSubmissionID = submissionIDs[1].toNumber();
         let submissionTwo = await selfCommitment.getSubmissionById(secondSubmissionID);
 
-        // console.log(submissionTwo);
-
         assert.equal(submissionTwo[0].toNumber(), 0, "Challenge ID is 0");
         assert.equal(submissionTwo[1], "url2", "URL of submission 2 not stored correctly");
         assert.equal(submissionTwo[2], "something2", "Description of submission 2 not stored correctly");
-        // assert.equal(submissionTwo[3], "url2", "URL of submission 2 not stored correctly");
-
-        // console.log(submissionTwo[3]);
-        // console.log(submissionTwo[3].toNumber());
-        var time = submissionTwo[3].toNumber();
-        console.log("_______")
-        console.log(now - time);
-        console.log("_______")
-
+        assert.closeTo(submissionTwo[3].toNumber(), now, minute, "Timestamp should be equal (very close up to a minute) to the current time");
         assert.equal(submissionTwo[4].toNumber(), 0, "state should be initial");
     });
 
     it('Should create a three challenges and craete two submissions to the second one and challenge then', async () => {
-        await selfCommitment.createChallenge("21 pushups", now - 4*day, now - 4*day, 20, { from: creator1, value: 18**10 });
-        await selfCommitment.createChallenge("22 pushups", now - 4*day, now - 4*day, 20, { from: creator2, value: 18**10 });
-        await selfCommitment.createChallenge("23 pushups", now - 4*day, now - 4*day, 20, { from: creator1, value: 18**10 });
-        await selfCommitment.createSubmission("url",  "something",  2, { from: creator2 });
-        await selfCommitment.createSubmission("url2", "something2", 2, { from: creator2 });
+        await selfCommitment.createChallenge("21 pushups", now - 4*day, now - 4*day, 20, { from: creator1, value: ETH });
+        await selfCommitment.createChallenge("22 pushups", now - 4*day, now - 4*day, 20, { from: creator2, value: ETH });
+        await selfCommitment.createChallenge("23 pushups", now - 4*day, now - 4*day, 20, { from: creator1, value: ETH });
+        await selfCommitment.createSubmission("url",  "something",  1, { from: creator2 });
+        await selfCommitment.createSubmission("url2", "something2", 1, { from: creator2 });
 
-
-        // Only creator of the challenge can create submission
-        console.log("before create")
-        await expectThrow( selfCommitment.createSubmission("url2", "something2", 2, { from: creator1 }) );
-        console.log("after create")
-
+        // Only creator of the challenge can create submission, expecting throw
+        await expectThrow( selfCommitment.createSubmission("url2", "something2", 1, { from: creator1 }) );
 
         // TODO: Currently on the front-end only 0 is supported
-        // Fee is transferred
+        let arbitrationCost = (await arbitrator.arbitrationCost([])).toNumber();
 
-
+        let disputedSubmissionID = 1;
+        await selfCommitment.disputeSubmission(disputedSubmissionID, "url", "url", { from: troll, value: arbitrationCost })
+        let s = await selfCommitment.getSubmissionById(disputedSubmissionID);
+        assert.equal(s[4].toNumber(), 1, "state should be challenged");
+        
+        await arbitrator.giveRuling(0, 1, { from: arbitratorOwner });
+        s = await selfCommitment.getSubmissionById(disputedSubmissionID);
+        assert.equal(s[4].toNumber(), 2, "state should be accepted");
 
 
 
